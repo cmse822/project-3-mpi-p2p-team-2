a405dee2dfdfc362ca5c0a31397a36cf46db5c4d
esteban <echeve11@msu.edu>
2024-03-05 10:35:33 -0500

    update script

--- submitRing.py
+++ submitRing.py
@@ -1,23 +1,22 @@
 import os
 for x in (2**p for p in range(1, 3)):
-    parameters_string = f"""
-    #!/bin/bash
-    ########## Define Resources Needed with SBATCH Lines ##########
-    
-    #SBATCH --time=04:00:00
-    #SBATCH --nodes=2         
-    #SBATCH --ntasks-per-node={int(x/2)}                 
-    #SBATCH --mem=100MB                    
-    #SBATCH --job-name RingSameNode
-    """
+    parameters_string = f"""#!/bin/bash
+########## Define Resources Needed with SBATCH Lines ##########
+
+#SBATCH --time=04:00:00
+#SBATCH --nodes=2         
+#SBATCH --ntasks-per-node={int(x/2)}                 
+#SBATCH --mem=100MB                    
+#SBATCH --job-name RingSameNode
+"""
 
     compilation_string1="""
-    MessageSize=(2 4 8 16 32 64 128 256 512 1024 2048 4096)
-    for Size in "${MessageSize[@]}"; do"""
+MessageSize=(2 4 8 16 32 64 128 256 512 1024 2048 4096)
+for Size in "${MessageSize[@]}"; do"""
     compilation_string2=f"""
-        mpiexec -n "$task" RingBlocking  "$Size" "100" data/Ring_{int(x)}.csv
-    done
-    """
+    mpiexec -n "$task" RingBlocking  "$Size" "100" data/Ring_{int(x)}.csv
+done
+"""
     with open(f"submitRing_{x}.sb", "w") as file:
         # Writing data to a file
         file.write(parameters_string)
